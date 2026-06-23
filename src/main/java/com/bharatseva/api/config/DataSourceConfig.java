159410b438575ddb2c29ef03043e26fd3a80c8ab
NithinKumar-7893 <nithinkumarbadduluri@gmail.com>
2026-06-23 15:47:16 +0530

    Updated properties file

---
+++ src/main/java/com/bharatseva/api/config/DataSourceConfig.java
@@ -0,0 +1,30 @@
+package com.bharatseva.api.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+import java.net.URI;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        String dbUrl = System.getenv("DATABASE_URL");
+        if (dbUrl == null || !dbUrl.startsWith("postgres://")) {
+            throw new RuntimeException("DATABASE_URL not set correctly");
+        }
+        
+        URI dbUri = URI.create(dbUrl);
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String jdbcUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        return DataSourceBuilder.create()
+                .url(jdbcUrl)
+                .username(username)
+                .password(password)
+                .build();
+    }
+}
